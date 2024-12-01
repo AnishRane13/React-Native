2c45d9a06ebb7cb1fcd4cf54a200be44e3a37566
AnishRane13 <anishrane178@gmail.com>
2024-12-01 16:48:24 +0530

    Reanimation done

--- app/about.jsx
+++ app/about.jsx
@@ -1,18 +1,33 @@
 import { View, Text, StyleSheet, Pressable } from "react-native";
 import React from "react";
 import ".././global.css";
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
+import Animated, { FadeIn, FadeOut, SlideInRight, SlideOutLeft } from "react-native-reanimated";
 
 const About = () => {
+  const router = useRouter();
+
   return (
-    <View className="flex-1 justify-center items-center bg-white">
-    <Text className="text-blue-500 text-3xl">About</Text>
-    <Link href='/' asChild>
-    <Pressable className='bg-blue-600 p-4 rounded-2xl'>
-      <Text className='text-white text-center'>Go To Home</Text>
-    </Pressable>
-    </Link>
-  </View>
+    <Animated.View 
+    entering={FadeIn.duration(500)}
+    exiting={FadeOut.duration(500)}
+className="flex-1 justify-center items-center bg-white">
+  <Text className="text-blue-500 text-3xl">About Page</Text>
+  {/* <Link href='/about' asChild>
+  <Pressable className='bg-blue-600 p-4 rounded-2xl'>
+    <Text className='text-white text-center'>Go To About</Text>
+  </Pressable>
+  </Link> */}
+
+  <Pressable
+    className="bg-blue-600 p-4 rounded-2xl"
+    onPress={() => {
+      router.push("/");
+    }}
+  >
+    <Text className="text-white text-center">Go to Home</Text>
+  </Pressable>
+</Animated.View>
   )
 }
 
--- app/index.jsx
+++ app/index.jsx
@@ -1,18 +1,34 @@
 import { View, Text, StyleSheet, Pressable } from "react-native";
 import React from "react";
 import ".././global.css";
-import { Link } from "expo-router";
+import { useRouter } from "expo-router";
+import Animated, { FadeIn, FadeOut, SlideInRight, SlideOutLeft } from "react-native-reanimated";
 
 const Home = () => {
+  const router = useRouter();
+
   return (
-    <View className="flex-1 justify-center items-center bg-white">
-      <Text className="text-blue-500 text-3xl">Homee</Text>
-      <Link href='/about' asChild>
+    <Animated.View 
+        entering={FadeIn.duration(500)}
+        exiting={FadeOut.duration(500)}
+    className="flex-1 justify-center items-center bg-white">
+      <Text className="text-blue-500 text-3xl">Homeeeeee</Text>
+      {/* <Link href='/about' asChild>
       <Pressable className='bg-blue-600 p-4 rounded-2xl'>
         <Text className='text-white text-center'>Go To About</Text>
       </Pressable>
-      </Link>
-    </View>
+      </Link> */}
+
+
+      <Pressable
+        className="bg-red-600 p-4 rounded-2xl"
+        onPress={() => {
+          router.push("/about");
+        }}
+      >
+        <Text className="text-white text-center">Go to About</Text>
+      </Pressable>
+    </Animated.View>
   );
 };
 
